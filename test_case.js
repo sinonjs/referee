47b0dfd990b0d7bfaea6378e257b723315adc597
Christian Johansen <christian@shortcut.no>
2010-10-24 21:18:45 +0200

    Visuals update for test_case utility

--- test_case.js
+++ test_case.js
@@ -9,16 +9,22 @@ function green(str) {
 };
 
 exports.testCase = function (name, tests) {
+    var testCount = 0;
+    var failCount = 0;
     sys.puts("\n" + name + "\n----------------------------------------");
 
     for (var test in tests) {
         sys.print(test);
+        testCount += 1;
 
         try {
             tests[test]();
             sys.print(" " + green("OK") + "\n");
         } catch (e) {
-            sys.print(" " + red("BUSTED") + " " + e + "\n");
+            sys.print(" " + red("BUSTED") + "\n    " + e + "\n");
+            failCount += 1;
         }
     }
+
+    sys.puts("\n" + testCount + " tests, " + (failCount ? failCount + " failed" : "all green"));
 };
