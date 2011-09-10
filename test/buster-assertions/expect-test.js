f2dac897d571acceed55960ff2d2ea3a0249ce9d
Christian Johansen <christian@gitorious.org>
2011-09-10 23:21:47 +0200

    Working expectations

--- test/buster-assertions/expect-test.js
+++ test/buster-assertions/expect-test.js
@@ -7,100 +7,121 @@ if (typeof require != "undefined") {
     var testHelper = require("../test-helper");
 
     var buster = {
-        assert: require("../../lib/buster-assertions"),
+        assertions: require("../../lib/buster-assertions"),
         util: require("buster-util")
     };
 
-    buster.expect = require("../../lib/buster-assertions/expect");
+    buster.expect = buster.assertions.expect;
 }
+
+var expect = buster.expect;
 
 buster.util.testCase("ExpectTest", {
     setUp: testHelper.setUp,
     tearDown: testHelper.tearDown,
 
     "should be function": function () {
-        //assert.equal(typeof buster.expect, "function");
+        assert.equal(typeof expect, "function");
     },
 
     "should bind assert.equals to argument": function () {
-        // assert.doesNotThrow(function () {
-        //     buster.expect({}).toEqual({});
-        // });
-    }// ,
+        assert.doesNotThrow(function () {
+            expect({}).toEqual({});
+        });
+    },
 
-    // "should fail assertion if a not equals b": function () {
-    //     try {
-    //         buster.expect({ id: 42 }).toEqual({ bleh: "Nah" });
-    //         throw new Error("Did not throw");
-    //     } catch (e) {
-    //         assert.equal(e.message, "[expect.toEqual] Expected [object Object] to be equal to [object Object]");
-    //     }
-    // },
+    "should fail assertion if a not equals b": function () {
+        try {
+            expect({ id: 42 }).toEqual({ bleh: "Nah" });
+            throw new Error("Did not throw");
+        } catch (e) {
+            assert.equal(e.message, "[expect.toEqual] Expected [object Object] to be equal to [object Object]");
+        }
+    },
 
-    // "should fail assertion by calling buster.assert.fail": function () {
-    //     sinon.stub(buster.assert, "fail");
+    "should fail assertion by calling buster.assert.fail": function () {
+        try {
+            expect({ id: 42 }).toEqual({ bleh: "Nah" });
+        } catch (e) {}
 
-    //     try {
-    //         buster.expect({ id: 42 }).toEqual({ bleh: "Nah" });
-    //     } catch (e) {}
+        assert.ok(buster.assertions.fail.calledOnce);
+    },
 
-    //     assert.ok(buster.assert.fail.calledOnce);
-    // },
+    "should emit assertion pass event": function () {
+        var listener = sinon.spy();
+        buster.assertions.on("pass", listener);
 
-    // "should emit assertion pass event": function () {
-    //     var listener = sinon.spy();
-    //     buster.assert.on("pass", listener);
+        expect({ id: 42 }).toEqual({ id: 42 });
 
-    //     buster.expect({ id: 42 }).toEqual({ id: 42 });
+        assert.ok(listener.calledOnce);
+    },
 
-    //     assert.ok(listener.calledOnce);
-    // },
+    "should emit assertion fail event": function () {
+        var listener = sinon.spy();
+        buster.assertions.on("failure", listener);
+        buster.assertions.throwOnFailure = false;
 
-    // "should emit assertion fail event": function () {
-    //     var listener = sinon.spy();
-    //     buster.assert.on("failure", listener);
-    //     buster.assert.throwOnFailure = false;
+        expect({ id: 42 }).toEqual({ id: 22 });
 
-    //     buster.expect({ id: 42 }).toEqual({ id: 22 });
+        assert.ok(listener.calledOnce);
+    },
 
-    //     assert.ok(listener.calledOnce);
-    // },
+    "should expose refutation as expectation too": function () {
+        try {
+            expect({ id: 42 }).not().toEqual({ id: 42 });
+            throw new Error("Did not throw");
+        } catch (e) {
+            assert.equal(e.message, "[expect.not.toEqual] Expected [object Object] not to be equal to [object Object]");
+        }
+    },
 
-    // "should expose all buster.assert methods": function () {
-    //     var expect = buster.expect;
-    //     var obj = { id: 42 };
+    "should allow double not": function () {
+        expect({ id: 42 }).not().not().toEqual({ id: 42 });
+    },
 
-    //     expect(true).toBeTrue();
-    //     expect(false).toBeFalse();
-    //     expect(obj).toBeSameAs(obj);
-    //     expect(obj).toNotBeSameAs({ id: 42 });
-    //     expect(obj).toEqual({ id: 42 });
-    //     expect(obj).toNotEqual({});
-    //     expect(obj).toBeType("object");
-    //     expect(obj).notToBeType("string");
-    //     expect("Something").toBeString();
-    //     expect(obj).toBeObject();
-    //     expect(function () {}).toBeFunction();
-    //     expect(true).toBeBoolean();
-    //     expect(42).toBeNumber();
-    //     expect(undefined).toBeUndefined();
-    //     expect(null).notToBeUndefined();
-    //     expect(null).toBeNull();
-    //     expect(42).notToBeNull();
-    //     expect(NaN).toBeNaN();
-    //     expect(42).notToBeNaN();
-    //     expect([]).toBeArray();
-    //     expect({ length: 1, "0": 1}).notToBeArray();
-    //     expect(arguments).toBeArrayLike();
-    //     expect(obj).toMatch({ id: 42 });
-    //     expect(obj).notToMatch({ id: 37 });
-    //     expect(function () { throw new TypeError("Oops"); }).toThrow("TypeError");
-    //     expect(function () {}).notToThrow();
-    //     expect({ tagName: "li" }).toHaveTagName("li");
-    //     expect({ tagName: "ol" }).toNotHaveTagName("li");
-    //     expect({ className: "a b c" }).toHaveClassName("b");
-    //     expect({ className: "a b c" }).toNotHaveClassName("d");
-    //     expect(4.5).toBeInDelta(5, 1);
-    //     expect(1).toNotBeInDelta(6, 1);
-    // }
+    "should expose all assertions": function () {
+        var expect = buster.expect;
+        var obj = { id: 42 };
+
+        expect(true).toBeTrue();
+        expect(false).not().toBeTrue();
+        expect(false).toBeFalse();
+        expect(true).not().toBeFalse();
+        expect(obj).toBeSameAs(obj);
+        expect(obj).not().toBeSameAs({ id: 42 });
+        expect(obj).toEqual({ id: 42 });
+        expect(obj).not().toEqual({});
+        expect(obj).toBeType("object");
+        expect(obj).not().toBeType("string");
+        expect("Something").toBeString();
+        expect([]).not().toBeString();
+        expect(obj).toBeObject();
+        expect(false).not().toBeObject();
+        expect(function () {}).toBeFunction();
+        expect({}).not().toBeFunction();
+        expect(true).toBeBoolean();
+        expect(42).not().toBeBoolean();
+        expect(42).toBeNumber();
+        expect("Hey").not().toBeNumber();
+        expect(undefined).toBeUndefined();
+        expect(null).not().toBeUndefined();
+        expect(null).toBeNull();
+        expect(42).not().toBeNull();
+        expect(NaN).toBeNaN();
+        expect(42).not().toBeNaN();
+        expect([]).toBeArray();
+        expect({ length: 1, "0": 1}).not().toBeArray();
+        expect(arguments).toBeArrayLike();
+        expect({}).not().toBeArrayLike();
+        expect(obj).toMatch({ id: 42 });
+        expect(obj).not().toMatch({ id: 37 });
+        expect(function () { throw new TypeError("Oops"); }).toThrow("TypeError");
+        expect(function () {}).not().toThrow();
+        expect({ tagName: "li" }).toHaveTagName("li");
+        expect({ tagName: "ol" }).not().toHaveTagName("li");
+        expect({ className: "a b c" }).toHaveClassName("b");
+        expect({ className: "a b c" }).not().toHaveClassName("d");
+        expect(4.5).toBeInDelta(5, 1);
+        expect(1).not().toBeInDelta(6, 1);
+    }
 });
