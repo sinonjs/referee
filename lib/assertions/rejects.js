sinonjs/referee

commit 1af3c778914f044e601ecfaed7dcb070d301e53c

feat(assertion) add resolves/rejects assertions (#45)
* feat(assertions): add resolves/reject

feat(assertion): attempt to add resolves/reject

some experiments so far

feat(assertion): improve resolves assertion and tests

Improve the basic implementation to work in a first stage.
This commit also adds some tests for the assertion.
The gneral assertion handling eas also touched, because an assertion
that returns a promise will return this value to the very end now.
The `resolves` assertion uses samsam's `identical` method to determine
if the given values are equal.

See: #27

feat(assertion): add refute method for `resolves` assertion

This is basically just copied from the `assert` method with negated
condition to pass (samsam's `identical`). Should be later refactored.
Also add some test that prove the behaviour.

See #27

refactor(assertion): add test file for `resolves`

Extract all test regarding the `resolves` assertion into its own test
file next to the source.

See #27

feat(assertion): add rejects

This is more or less a copy of the resolves assertion where the
then/catch functions are switched. Also add some basic tests

See #27

feat(docs): add basics about resolves/rejects

See #27

* refactor(assertions): resolves/rejects

Refactor the resolves/rejects assertions to avoid code duplication.
Introduce a new function that creates asynchronous assertions. This
leaves the old code commented for reference.

See #27

* refactor: changes according to review

* remove commented out code
* wrap tests in context
* don't expose messge properties, set as variable
* improve assert/refute messages
* clearer documentation

Thanks to @mantoni @mroderick

See # 27

lib/assertions/rejects.js
@@ -1,0 +1,31 @@
+"use strict";
+
+var samsam = require("@sinonjs/samsam");
+var createAsyncAssertion = require("../create-async-assertion");
+
+var assertMessage = "${actual} is not identical to ${expected}";
+var refuteMessage = "${actual} is identical to ${expected}";
+
+module.exports = function(referee) {
+    function thenCallback() {
+        this.reject("${0} did not reject, it resolved instead");
+    }
+    referee.add("rejects", {
+        assert: createAsyncAssertion(thenCallback, function(actual, expected) {
+            if (!samsam.identical(actual, expected)) {
+                this.reject(assertMessage);
+                return;
+            }
+            this.resolve();
+        }),
+        refute: createAsyncAssertion(thenCallback, function(actual, expected) {
+            if (samsam.identical(actual, expected)) {
+                this.reject(refuteMessage);
+                return;
+            }
+            this.resolve();
+        }),
+        assertMessage: assertMessage,
+        refuteMessage: refuteMessage
+    });
+};

lib/assertions/resolves.js
@@ -1,0 +1,31 @@
+"use strict";
+
+var samsam = require("@sinonjs/samsam");
+var createAsyncAssertion = require("../create-async-assertion");
+
+var assertMessage = "${actual} is not identical to ${expected}";
+var refuteMessage = "${actual} is identical to ${expected}";
+
+module.exports = function(referee) {
+    function catchCallback() {
+        this.reject("${0} did not resolve, it rejected instead");
+    }
+    referee.add("resolves", {
+        assert: createAsyncAssertion(function(actual, expected) {
+            if (!samsam.identical(actual, expected)) {
+                this.reject(assertMessage);
+                return;
+            }
+            this.resolve();
+        }, catchCallback),
+        refute: createAsyncAssertion(function(actual, expected) {
+            if (samsam.identical(actual, expected)) {
+                this.reject(refuteMessage);
+                return;
+            }
+            this.resolve();
+        }, catchCallback),
+        assertMessage: assertMessage,
+        refuteMessage: refuteMessage
+    });
+};

lib/assertions/rejects.test.js
@@ -1,0 +1,94 @@
+"use strict";
+
+var referee = require("../referee");
+
+describe("assert.rejects", function() {
+    it("should return a promise", function() {
+        var assertion = referee.assert.rejects(Promise.reject("test"), "test");
+        referee.assert.isPromise(assertion);
+    });
+
+    context("when promise argument rejects to value argument", function() {
+        it("should resolve the returned promise", function() {
+            return referee.assert.rejects(Promise.reject("test"), "test");
+        });
+    });
+
+    context(
+        "when promise argument does not reject to value argument",
+        function() {
+            it("should reject the returned promise", function() {
+                return referee.assert
+                    .rejects(Promise.reject("test"), "test2")
+                    .catch(function(e) {
+                        referee.assert.isError(e);
+                    });
+            });
+        }
+    );
+
+    context("when promise argument is not a promise", function() {
+        it("should reject the returned promise", function() {
+            return referee.assert.rejects({}, "test").catch(function(e) {
+                referee.assert.isError(e);
+            });
+        });
+    });
+
+    context("when promise argument does not reject", function() {
+        it("should reject the returned promise", function() {
+            return referee.assert
+                .rejects(Promise.resolve(), "test")
+                .catch(function(e) {
+                    referee.assert.isError(e);
+                });
+        });
+    });
+});
+
+describe("refute.rejects", function() {
+    it("should return a promise", function() {
+        var refutation = referee.refute.rejects(
+            Promise.reject("test"),
+            "test2"
+        );
+        referee.assert.isPromise(refutation);
+    });
+
+    context(
+        "when promise argument does not rejects to value argument",
+        function() {
+            it("should resolve the returned promise", function() {
+                return referee.refute.rejects(Promise.reject("test"), "test2");
+            });
+        }
+    );
+
+    context("when promise argument rejects to value argument", function() {
+        it("should reject the returned promise", function() {
+            return referee.refute
+                .rejects(Promise.reject("test"), "test")
+                .catch(function(e) {
+                    referee.assert.isError(e);
+                });
+        });
+    });
+
+    context("when promise argument is not a promise", function() {
+        it("should reject the returned promise", function() {
+            return referee.refute.rejects({}, "test").catch(function(e) {
+                referee.assert.isError(e);
+            });
+        });
+    });
+
+    context("when promise argument does not reject", function() {
+        it("should reject the returned promise", function() {
+            return referee.refute
+                .rejects(Promise.resolve(), "test")
+                .catch(function(e) {
+                    referee.assert.isError(e);
+                });
+        });
+    });
+});

lib/assertions/resolves.test.js
@@ -1,0 +1,100 @@
+"use strict";
+
+var referee = require("../referee");
+
+describe("assert.resolves", function() {
+    it("should return a promise", function() {
+        var assertion = referee.assert.resolves(
+            Promise.resolve("test"),
+            "test"
+        );
+        referee.assert.isPromise(assertion);
+    });
+
+    context("when promise argument resolves to value argument", function() {
+        it("should resolve the returned promise", function() {
+            return referee.assert.resolves(Promise.resolve("test"), "test");
+        });
+    });
+
+    context(
+        "when promise argument does not resolves to value argument",
+        function() {
+            it("should reject the returned promise", function() {
+                return referee.assert
+                    .resolves(Promise.resolve("test"), "test2")
+                    .catch(function(e) {
+                        referee.assert.isError(e);
+                    });
+            });
+        }
+    );
+
+    context("when promise argument is not a promise", function() {
+        it("should reject the returned promise", function() {
+            return referee.assert.resolves({}, "test").catch(function(e) {
+                referee.assert.isError(e);
+            });
+        });
+    });
+
+    context("when promise argument does not resolve", function() {
+        it("should reject the returned promise", function() {
+            return referee.assert
+                .resolves(Promise.reject(), "test")
+                .catch(function(e) {
+                    referee.assert.isError(e);
+                });
+        });
+    });
+});
+
+describe("refute.resolves", function() {
+    it("should return a promise", function() {
+        var refutation = referee.refute.resolves(
+            Promise.resolve("test"),
+            "test"
+        );
+        referee.assert.isPromise(refutation);
+    });
+
+    context(
+        "when promise argument does not resolve to value argument",
+        function() {
+            it("should resolve the returned promise", function() {
+                return referee.refute.resolves(
+                    Promise.resolve("test"),
+                    "test2"
+                );
+            });
+        }
+    );
+
+    context("when promise argument resolves to value argument", function() {
+        it("should reject the returned promise", function() {
+            return referee.refute
+                .resolves(Promise.resolve("test"), "test")
+                .catch(function(e) {
+                    referee.assert.isError(e);
+                });
+        });
+    });
+
+    context("when promise argument is not a promise", function() {
+        it("should reject the returned promise", function() {
+            return referee.refute.resolves({}, "test").catch(function(e) {
+                referee.assert.isError(e);
+            });
+        });
+    });
+
+    context("when promise argument does not resolve", function() {
+        it("should reject the returned promise", function() {
+            return referee.refute
+                .resolves(Promise.reject(), "test")
+                .catch(function(e) {
+                    referee.assert.isError(e);
+                });
+        });
+    });
+});

lib/create-async-assertion.js
@@ -1,0 +1,33 @@
+"use strict";
+
+function createAsyncAssertion(thenFunc, catchFunc) {
+    function asyncAssertion(promise, expected) {
+        var self = this;
+        this.expected = expected;
+        function applyCallback(callback, context) {
+            return function(actual) {
+                self.actual = actual;
+                callback.apply(context, [actual, expected]);
+            };
+        }
+        var assertionPromise = new Promise(function(resolve, reject) {
+            try {
+                var context = { resolve: resolve, reject: reject };
+                promise.then(
+                    applyCallback(thenFunc, context),
+                    applyCallback(catchFunc, context)
+                );
+            } catch (error) {
+                reject(error.message);
+            }
+        }).catch(function(message) {
+            self.fail(message);
+        });
+
+        return assertionPromise;
+    }
+
+    return asyncAssertion;
+}
+
+module.exports = createAsyncAssertion;

lib/create-async-assertion.test.js
@@ -1,0 +1,67 @@
+"use strict";
+
+var referee = require("./referee");
+var sinon = require("sinon");
+var createAsyncAssertion = require("./create-async-assertion");
+var noop = function() {};
+
+describe("createAsyncAssertion", function() {
+    it("should return a Function", function() {
+        var func = createAsyncAssertion(noop, noop);
+        referee.assert.isFunction(func);
+    });
+
+    it("should result in a promise", function() {
+        var func = createAsyncAssertion(noop, noop);
+        var promise = func.call({}, Promise.resolve(), "test");
+        referee.assert.isPromise(promise);
+    });
+
+    it("should fail when input is not a promise", function() {
+        var fail = sinon.spy();
+        var func = createAsyncAssertion(noop, noop);
+        var context = {
+            fail: fail
+        };
+        var promise = func.call(context, {}, "test");
+        return promise.then(function() {
+            referee.assert.isTrue(
+                fail.calledOnceWith("promise.then is not a function")
+            );
+        });
+    });
+
+    describe("applyCallback", function() {
+        var resolve = function() {
+            this.resolve();
+        };
+        var thenCallback = sinon.spy(resolve);
+        var catchCallback = sinon.spy(resolve);
+        var func = createAsyncAssertion(thenCallback, catchCallback);
+
+        beforeEach(function() {
+            thenCallback.resetHistory();
+            catchCallback.resetHistory();
+        });
+
+        it("should apply and call the given `thenCallback`", function() {
+            var promise = func.call({}, Promise.resolve("test"), "test");
+            return promise.then(function() {
+                referee.assert.isTrue(
+                    thenCallback.calledOnceWith("test", "test")
+                );
+                referee.refute.isTrue(catchCallback.called);
+            });
+        });
+
+        it("should apply and call the given `catchCallback`", function() {
+            var promise = func.call({}, Promise.reject("test"), "test");
+            return promise.then(function() {
+                referee.assert.isTrue(
+                    catchCallback.calledOnceWith("test", "test")
+                );
+                referee.refute.isTrue(thenCallback.called);
+            });
+        });
+    });
+});

lib/define-assertion.js
@@ -60,7 +60,21 @@ function createAssertion(referee, type, name, func, minArgs, messageValues) {
             }
         };
 
-        if (!func.apply(ctx, arguments) && !failed) {
+        var result = func.apply(ctx, arguments);
+
+        if (result instanceof Promise) {
+            // Here we need to return the promise in order to tell test
+            // runners that this is an asychronous assertion.
+            // eslint complains about the return statement, because it is
+            // not expected here, so let's ignore.
+            // https://eslint.org/docs/rules/consistent-return
+            /* eslint-disable-next-line consistent-return */
+            return result.then(function() {
+                referee.pass(["pass", fullName].concat(args));
+            });
+        }
+
+        if (!result && !failed) {
             // when a function returns false and hasn't already failed with a custom message,
             // fail with default message
             ctx.fail("message");
@@ -97,7 +111,7 @@ function defineAssertion(
             minArgs,
             messageValues
         );
-        assertion.apply(null, arguments);
+        return assertion.apply(null, arguments);
     };
 }
 

lib/referee.js
@@ -86,5 +86,7 @@ require("./assertions/same")(referee);
 require("./assertions/tag-name")(referee);
 require("./assertions/json")(referee);
 require("./assertions/match-json")(referee);
+require("./assertions/resolves")(referee);
+require("./assertions/rejects")(referee);
 
 module.exports = referee;

commit 5522395c4b2b40e65aa80174d2c47cc6d845ec3e

Add missing expectation property for resolves/rejects
When these assertions were introduced in
1af3c778914f044e601ecfaed7dcb070d301e53c, we overlooked the expectation
property.

lib/assertions/rejects.js
@@ -25,6 +25,7 @@ module.exports = function(referee) {
             }
             this.resolve();
         }),
+        expectation: "toRejectWith",
         assertMessage: assertMessage,
         refuteMessage: refuteMessage
     });

lib/assertions/resolves.js
@@ -25,6 +25,7 @@ module.exports = function(referee) {
             }
             this.resolve();
         }, catchCallback),
+        expectation: "toResolveWith",
         assertMessage: assertMessage,
         refuteMessage: refuteMessage
     });