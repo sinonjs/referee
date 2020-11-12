8bc3b585ec86a714952166fa9ace36f2751c18ae
Morgan Roderick <morgan@roderick.dk>
2020-11-12 17:25:44 +0100

    Upgrade prettier to 2.x
    * upgrade `eslint-config-prettier`
    * upgrade `eslint-plugin-prettier`
    * run `prettier` on all JavaScript files
        ```sh
        $(npm bin)/prettier -w -u .`
        ```

--- demo/diff.test.js
+++ demo/diff.test.js
@@ -5,19 +5,19 @@
 
 var assert = require("..").assert;
 
-describe("diff", function() {
-    it("multiline string", function() {
-        assert.equals("foo\nbar\ndoo\n", "bar\ndoo\nxyz\n");
-    });
+describe("diff", function () {
+  it("multiline string", function () {
+    assert.equals("foo\nbar\ndoo\n", "bar\ndoo\nxyz\n");
+  });
 
-    it("objects", function() {
-        assert.equals({ foo: 42 }, { foo: 66 });
-    });
+  it("objects", function () {
+    assert.equals({ foo: 42 }, { foo: 66 });
+  });
 
-    it("matchJson", function() {
-        assert.matchJson('{"foo":42,"bar":true,"ignored":true}', {
-            foo: 42,
-            bar: false
-        });
+  it("matchJson", function () {
+    assert.matchJson('{"foo":42,"bar":true,"ignored":true}', {
+      foo: 42,
+      bar: false,
     });
+  });
 });
