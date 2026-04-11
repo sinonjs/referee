72527d7d5a9121cd6e2e526aa7e8fb3a9dc8376b
Carl-Erik Kopseng <carlerik@gmail.com>
2026-04-11 18:45:33 +0200

    Upgrade to breaking ESLint 10 and new shared config (#259)
    * Upgrade to breaking ESLint 10 and new shared config
    
    * ignore config
    
    * prettier

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+"use strict";
+
+const sinonConfig = require("@sinonjs/eslint-config");
+
+module.exports = [
+  {
+    ignores: [
+      "eslint.config.js",
+      "coverage/**",
+      "dist/**",
+      "docs/**",
+      "site/**",
+    ],
+  },
+  ...sinonConfig,
+  {
+    rules: {
+      "jsdoc/require-returns": "off",
+    },
+  },
+  {
+    files: ["lib/**/*.test.js"],
+    rules: {
+      "mocha/consistent-spacing-between-blocks": "off",
+      "no-unused-vars": [
+        "error",
+        { vars: "all", args: "after-used", caughtErrors: "none" },
+      ],
+    },
+  },
+  {
+    files: ["lib/assertions/json.js", "lib/assertions/match-json.js"],
+    rules: {
+      "no-unused-vars": "off",
+    },
+  },
+  {
+    languageOptions: {
+      globals: {
+        ArrayBuffer: false,
+        Uint8Array: false,
+      },
+    },
+  },
+];
